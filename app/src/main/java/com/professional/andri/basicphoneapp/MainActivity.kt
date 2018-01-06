700eed1c8c4e40e2eccb4a7d79749a7a00e4b924
Andri <wuandri@rocketmail.com>
2018-01-06 22:23:24 +0700

    Make phone call and its request permission

--- app/src/main/java/com/professional/andri/basicphoneapp/MainActivity.kt
+++ app/src/main/java/com/professional/andri/basicphoneapp/MainActivity.kt
@@ -1,14 +1,68 @@
 package com.professional.andri.basicphoneapp
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.Toast
+import android.content.pm.PackageManager
+import android.Manifest.permission
+import android.Manifest.permission.CALL_PHONE
+import android.support.v4.app.ActivityCompat
+import android.os.Build
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     private var callButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        callButton = findViewById(R.id.call_button) as Button
+        callButton!!.setOnClickListener{
+            isPermissionGranted()
+        }
+    }
+
+    fun isPermissionGranted(): Boolean {
+        if (Build.VERSION.SDK_INT >= 23) {
+            return if (checkSelfPermission(android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                Log.v("TAG", "Permission is granted")
+                val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "085275449512"))
+                startActivity(intent)
+                true
+            } else {
+
+                Log.v("TAG", "Permission is revoked")
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+                false
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("TAG", "Permission is granted")
+            return true
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+
+            1 -> {
+
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(applicationContext, "Permission granted", Toast.LENGTH_SHORT).show()
+//                    callPhone()
+                } else {
+                    Toast.makeText(applicationContext, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }// other 'case' lines to check for other
+        // permissions this app might request
     }
 }
